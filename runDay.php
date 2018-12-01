66eb001865311e48dd51d1e529876d00ccbead0d
Xeres <xeres666@googlemail.com>
2018-12-01 14:09:46 +0100

    safeguard against non existing classes being created

--- runDay.php
+++ runDay.php
@@ -6,10 +6,16 @@ require_once('vendor/autoload.php');
 // remove script file name
 array_shift($argv);
 
-foreach ($argv as $i => $parameter)
-{
+foreach ($argv as $i => $parameter) {
     if (is_numeric($parameter)) {
         $class = "AoC2018\Days\Day$parameter";
+
+        if (!file_exists("Days/Day$parameter.php")) {
+            $cli = new \AoC2018\Utility\Cli();
+            $cli->logLine("$class not (yet) implemented", $cli::$COLOR_RED);
+            continue;
+        }
+
         $day = new $class();
         $day->run();
     }
