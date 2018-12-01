22ae59b06fec53d5030c9a652d353d79cb801972
Xeres <xeres666@googlemail.com>
2018-12-01 14:30:47 +0100

    [day1] part 2

--- Days/Day1.php
+++ Days/Day1.php
@@ -10,15 +10,50 @@ class Day1 extends AbstractDay
     {
         $this->logLine("Day 1", self::$COLOR_GREEN);
 
+        $resultFrequency = $this->part1();
+        $repeatedFrequency = $this->part2();
+
+        $this->logLine("Result 1: $resultFrequency", self::$COLOR_GREEN);
+        $this->logLine("Result 2: $repeatedFrequency", self::$COLOR_LIGHT_GREEN);
+        $this->logLine("Time: ". $this->getDurationFormated(), self::$COLOR_YELLOW);
+
+    }
+
+    protected function part1()
+    {
+        $frequencies = $this->readinput();
+
         // Start with 0 and add every number
         $resultFrequency = 0;
-        $frequencies = $this->readinput();
         foreach ($frequencies as $frequency) {
             $resultFrequency += (int)$frequency;
         }
+        return $resultFrequency;
+    }
 
-        $this->logLine("Result: $resultFrequency", self::$COLOR_GREEN);
-        $this->logLine("Time: ". $this->getDurationFormated(), self::$COLOR_YELLOW);
+    protected function part2()
+    {
+        $frequencies = $this->readinput();
+        $currentFrequency = 0;
+
+        // record frequencies as array
+        $repeatedFrequencies = [$currentFrequency => 1];
+
+        // loop over the frequency changes endlessly
+        while (true) {
+            foreach ($frequencies as $frequency) {
+                $currentFrequency += (int)$frequency;
+
+                // is the frequency already known?
+                if (isset($repeatedFrequencies[$currentFrequency])) {
+                    // it occurred twice then!
+                    return $currentFrequency;
+                } else {
+                    // remember this frequency
+                    $repeatedFrequencies[$currentFrequency] = 1;
+                }
+            }
+        }
     }
 
     protected function readinput()
