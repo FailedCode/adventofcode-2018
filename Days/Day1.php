9416423637e84708fbb9eaaec95786ea68155da7
Xeres <xeres666@googlemail.com>
2018-12-01 14:13:22 +0100

    [day1] part1 solution

--- Days/Day1.php
+++ Days/Day1.php
@@ -9,5 +9,21 @@ class Day1 extends AbstractDay
     public function run()
     {
         $this->logLine("Day 1", self::$COLOR_GREEN);
+
+        // Start with 0 and add every number
+        $resultFrequency = 0;
+        $frequencies = $this->readinput();
+        foreach ($frequencies as $frequency) {
+            $resultFrequency += (int)$frequency;
+        }
+
+        $this->logLine("Result: $resultFrequency", self::$COLOR_GREEN);
+        $this->logLine("Time: ". $this->getDurationFormated(), self::$COLOR_YELLOW);
+    }
+
+    protected function readinput()
+    {
+        $file = $this->config['input_path'] . 'day1.txt';
+        return explode("\n", file_get_contents($file));
     }
 }
