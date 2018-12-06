34f9d5d0e3c6d2163ed39cbe82a9a6d7f4909177
Xeres <xeres666@googlemail.com>
2018-12-06 16:24:13 +0100

    [day5] part 2

--- Days/Day5.php
+++ Days/Day5.php
@@ -10,14 +10,23 @@ class Day5 extends AbstractDay
 
     protected function part1()
     {
+        $polymer = $this->retractPolymer($this->readinput(), $this->generateReplacements());
+        return strlen($polymer);
+    }
+
+    protected function generateReplacements()
+    {
         $replacements = [];
         for ($i = 65; $i < 91; $i++) {
             $char = chr($i);
             $replacements[] = $char . strtolower($char);
             $replacements[] = strtolower($char) . $char;
         }
+        return $replacements;
+    }
 
-        $polymer = $this->readinput();
+    protected function retractPolymer($polymer, $replacements)
+    {
         while (true) {
             $lengthLast = strlen($polymer);
             $polymer = str_replace($replacements, '', $polymer);
@@ -25,12 +34,29 @@ class Day5 extends AbstractDay
                 break;
             }
         }
-
-        return $lengthLast;
+        return $polymer;
     }
 
     protected function part2()
     {
+        $replacements = [];
+        for ($i = 65; $i < 91; $i++) {
+            $char = chr($i);
+            $replacements[] = [
+                $char,
+                strtolower($char)
+            ];
+        }
+
+        $polymer = $this->readinput();
+        $minimumCounts = [];
+        foreach ($replacements as $replacement) {
+            $workPolymer = $this->retractPolymer($polymer, $replacement);
+            $workPolymer = $this->retractPolymer($workPolymer, $this->generateReplacements());
+            $minimumCounts[] = strlen($workPolymer);
+        }
+
+        return min($minimumCounts);
     }
 
     protected function readinput()
