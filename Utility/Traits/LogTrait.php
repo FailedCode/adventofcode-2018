26c66e275ade62a6088b64e19f37a1b435284232
Xeres <xeres666@googlemail.com>
2018-12-08 08:25:26 +0100

    text logging with easy color changes in text

--- Utility/Traits/LogTrait.php
+++ Utility/Traits/LogTrait.php
@@ -21,16 +21,43 @@ trait LogTrait
     public static $COLOR_LIGHT_GRAY = 'light_gray';
     public static $COLOR_WHITE = 'white';
 
-    public function logLine($text = '', $color = null)
+    /**
+     * Display text with [<color>] interpreted
+     *
+     * @param $text
+     */
+    public function logText($text)
     {
+        $lines = explode("\n", $text);
+        $output = [];
+        $color = false;
+
+        foreach ($lines as $line) {
+            $pattern = '~(\[[^]]*\])~';
+            $parts = preg_split($pattern, $line, -1, PREG_SPLIT_DELIM_CAPTURE);
+
+            foreach ($parts as $part) {
+                if (preg_match($pattern, $part)) {
+                    $color = $this->getColorAlias(str_replace(['[', ']'], '', $part));
+                    continue;
+                }
+                $output[] = $this->colorString($part, $color);
+            }
+            $output[] = "\n";
+        }
+        $this->log(implode("", $output));
+    }
+
+    public function logLine($data, $color = null)
+    {
+        $text = $this->formatLog($data);
         $this->log("$text\n", $color);
     }
 
     public function logLines($text)
     {
         if (is_array($text)) {
-            foreach ($text as $line => $color)
-            {
+            foreach ($text as $line => $color) {
                 $this->logLine($line, $color);
             }
             return;
@@ -85,14 +112,32 @@ trait LogTrait
         return (string)$data;
     }
 
-    /**
-     * Wrap a string in color codes for unix consoles
-     *
-     * @param string $str
-     * @param string $color
-     * @return string
-     */
-    public function colorString($str, $color = 'white')
+    protected function getColorAlias($color)
+    {
+        $color = strtolower($color);
+        $aliases = [
+            'r' => 'red',
+            'g' => 'green',
+            'b' => 'blue',
+            'lr' => 'light_red',
+            'lg' => 'light_green',
+            'lb' => 'light_blue',
+            'lc' => 'light_cyan',
+            'lp' => 'light_purple',
+            'dg' => 'dark_gray',
+            'p' => 'purple',
+            'y' => 'yellow',
+            'w' => 'white',
+            'x' => 'black',
+            '' => 'light_gray',
+        ];
+        if (isset($aliases[$color])) {
+            return $aliases[$color];
+        }
+        return $color;
+    }
+
+    protected function getColor($color)
     {
         $colors = [
             'black' => '0;30',
@@ -112,10 +157,26 @@ trait LogTrait
             'light_gray' => '0;37',
             'white' => '1;37',
         ];
-        if (!isset($colors[$color])) {
-            return $str;
+        if (isset($colors[$color])) {
+            return $colors[$color];
         }
-        return "\033[${colors[$color]}m$str\033[0m";
+        return false;
     }
 
+    /**
+     * Wrap a string in color codes for unix consoles
+     *
+     * @param string $str
+     * @param string $color
+     * @return string
+     */
+    public function colorString($str, $color = 'white')
+    {
+        $color = $this->getColor($color);
+        if (!$color) {
+            return $str;
+        }
+        return "\033[{$color}m{$str}\033[0m";
+
+    }
 }
--- runDay.php
+++ runDay.php
@@ -8,13 +8,14 @@ array_shift($argv);
 $cli = new \AoC2018\Utility\Cli();
 
 if (array_search('--help', $argv) !== false) {
-    $cli->logLines([
-        "Usage: " => $cli::$COLOR_YELLOW,
-        "./runDay.php 1 => run day 1" => $cli::$COLOR_WHITE,
-        "./runDay.php 1 2 3 => run days 1, 2 and 3" => $cli::$COLOR_WHITE,
-        "./runDay.php 2p1 => run day 2 only part 1" => $cli::$COLOR_WHITE,
-        "./runDay.php 2p2 => run day 2 only part 2" => $cli::$COLOR_WHITE,
-    ]);
+    $cli->logText(
+        "[Y]Usage: \n" .
+        "[W]./runDay.php 1[] => run day 1\n" .
+        "[W]./runDay.php 1 2 3[] => run days 1, 2 and 3\n" .
+        "[W]./runDay.php 2p1[] => run day 2 only part 1\n" .
+        "[W]./runDay.php 2p2[] => run day 2 only part 2\n" .
+        "[W]./runDay.php 2 --test[] => test day 2 results"
+    );
     exit(0);
 }
 
