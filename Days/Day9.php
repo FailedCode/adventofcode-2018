7868a953b823d4a24d1ea51d03c25ed68e320511
Xeres <xeres666@googlemail.com>
2018-12-09 13:36:25 +0100

    [day9] part 1

---
+++ Days/Day9.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace AoC2018\Days;
+
+
+class Day9 extends AbstractDay
+{
+    protected $title = 'Marble Mania';
+
+    protected function part1()
+    {
+        $input = $this->readinput();
+        $playerNumber = $input['players'];
+        $maxMarbles = $input['marbles'];
+
+        $players = array_fill(0, $playerNumber, 0);
+        $marbles = [0 => 0];
+        $currentMarble = 0;
+        $currentPlayer = 0;
+
+        for ($i = 1; $i < $maxMarbles + 1; $i++) {
+            $marbleCount = count($marbles);
+
+            if (($i % 23) == 0) {
+                $currentMarble = ($currentMarble - 7 + $marbleCount) % $marbleCount;
+                $players[$currentPlayer] += $i + $marbles[$currentMarble];
+
+                array_splice($marbles, $currentMarble, 1);
+
+//                $marble = $marbles[$currentMarble];
+//                $this->logText(str_replace(" $marble ", $this->colorString(" ($marble) ", 'blue'), implode(' ', $marbles) . ' '));
+            } else {
+                $currentMarble = ($currentMarble + 1) % $marbleCount;
+                array_splice($marbles, $currentMarble + 1, 0, $i);
+                $currentMarble = array_keys($marbles, $i)[0];
+//                $this->logText(str_replace(" $i ", $this->colorString(" ($i) ", 'blue'), (implode(' ', $marbles) . ' ')));
+
+            }
+            $currentPlayer = ($currentPlayer + 1) % $playerNumber;
+        }
+
+        return max($players);
+    }
+
+    protected function part2()
+    {
+    }
+
+    protected function readinput()
+    {
+        $file = $this->getInputFile();
+        preg_match('~(\d+).*?(\d+).*~', file_get_contents($file), $matches);
+        return ['players' => $matches[1], 'marbles' => $matches[2]];
+    }
+}
